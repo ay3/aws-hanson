d088dddc703273685cc170e99ed81c1700a6cd21
ay <draft@perl.pink>
2020-09-18 21:10:44 +0900

    Create complehend-function-api.py
    use API Gateway

---
+++ lambda/complehend-function-api.py
@@ -0,0 +1,20 @@
+import json
+import boto3
+
+comprehend = boto3.client('comprehend')
+
+def lambda_handler(event, context):
+    input_text = event['queryStringParameters']['input_text']
+    
+    response = comprehend.detect_sentiment(
+        Text=input_text,
+        LanguageCode='ja'
+    )
+    sentiment_score = response['SentimentScore']
+    # TODO implement
+    return {
+        'statusCode': 200,
+        'body': json.dumps({
+            'sentiment_score' : sentiment_score
+        })
+    }
